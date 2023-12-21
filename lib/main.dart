a260ddacd069f71590836414722e4f34afbba7d3
Bilawal-Mehfooz-Malik <bilawalmehfoozflutter@gmail.com>
2023-12-21 11:04:43 +0500

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:word_hurdle_puzzle/screens/word_hurdle_screen.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:word_hurdle_puzzle/providers/hurdle_provider.dart';
+import 'package:word_hurdle_puzzle/screens/word_hurdle_screen.dart';
 
 final _colorScheme = ColorScheme.fromSeed(
     brightness: Brightness.dark, seedColor: Colors.deepPurple);
